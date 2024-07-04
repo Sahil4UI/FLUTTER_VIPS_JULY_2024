df3d163075d930c83ab304e513b7ac86c5952020
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2024-07-04 11:48:48 +0530

    Update TicTacToe.dart

--- DART/TicTacToe.dart
+++ DART/TicTacToe.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 List board = List.generate(9,(int index)=>index+1);
+List temp = [...board];
+List WinningCases = [[0,1,2],[3,4,5],[6,7,8],
+                    [0,3,6],[1,4,7],[2,5,8],
+                      [0,4,8],[2,4,6]];
 showBoard()
 {
     for (int i=0;i<board.length;i++)
@@ -15,18 +19,41 @@ showBoard()
 }
 playerTurn(choice,int pos)
 {
+    temp.remove(pos);
+   
     board[pos-1]=choice;
+    showBoard();
+    return checkwinners(choice);
 }
 
-cpuTurn()
+cpuTurn(cpu_choice)
 {
-
+  print("**********CPU TURN*******");
+    temp.shuffle();
+    int cpu_pos=temp[0];
+    board[cpu_pos-1] = cpu_choice;
+    temp.removeAt(0);
+     print(temp);
+    showBoard();
+    return checkwinners(cpu_choice);
 }
 
-checkwinners()
+checkwinners(choice)
 {
-
+    for (List wins in WinningCases)
+    {
+        if (board[wins[0]]==choice &&
+         board[wins[1]]==choice && 
+         board[wins[2]]==choice)
+         {
+              print("$choice is Winner");
+              return "wins";
+              
+         }
+    }
+    return 0;
 }
+
 void main()
 {
    showBoard();
@@ -44,7 +71,10 @@ void main()
    while(true){
     print("Enter Player Pos : ");
     player_pos = int.parse(stdin.readLineSync()!);
-    playerTurn(player_choice, player_pos);
-    showBoard();
+    if (playerTurn(player_choice, player_pos)=="wins")
+    {
+          break;
+    }
+    cpuTurn(cpu_choice);
    }
 }
