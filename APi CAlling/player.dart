59e7d954d4c36fdc8e5d0eb66ff2b1116aba00b1
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2024-07-18 13:05:02 +0530

    Add files via upload

--- APi CAlling/player.dart
+++ APi CAlling/player.dart
@@ -9,32 +9,47 @@ class Player extends StatefulWidget {
 }
 
 class _PlayerState extends State<Player> {
+  var data;
   MatchService obj = MatchService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Icon(Icons.sports_cricket,size: 50,),
-        title:const Text("CrickPedia",
-        style: TextStyle(
-          fontSize: 30
-        ),),
+        leading: const Icon(
+          Icons.sports_cricket,
+          size: 50,
+        ),
+        title: const Text(
+          "CrickPedia",
+          style: TextStyle(fontSize: 30),
+        ),
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(80),
-          child:Padding(
+          child: Padding(
             padding: EdgeInsets.all(8.0),
             child: TextField(
-                decoration: InputDecoration(
+              decoration: InputDecoration(
                   hintText: "Enter Player Name",
-                  hintStyle: TextStyle(
-                    fontSize: 30
-                  )
-                ),
-                onSubmitted: (value)
-                {
-                    obj.getId(value);
-                },
+                  hintStyle: TextStyle(fontSize: 30)),
+              onSubmitted: (value) async {
+                data = await obj.getId(value);
+                setState(() {});
+              },
             ),
+          ),
+        ),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              (data == null)
+                  ? const CircularProgressIndicator()
+                  : CircleAvatar(
+                      backgroundImage: NetworkImage(data["data"]["playerImg"]),
+                      radius: 150,
+                    )
+            ],
           ),
         ),
       ),
--- APi CAlling/services/match_service.dart
+++ APi CAlling/services/match_service.dart
@@ -1,11 +1,19 @@
 import 'package:dio/dio.dart';
 
-class MatchService{
+class MatchService {
   final dio = Dio();
-  String id_path = "https://api.cricapi.com/v1/players?apikey=a7233f4d-1f47-461a-92d5-899b8409091d&offset=0&search=";
+  String id_path =
+      "https://api.cricapi.com/v1/players?apikey=a7233f4d-1f47-461a-92d5-899b8409091d&offset=0&search=";
 
-void getId(String name) async {
-  final response = await dio.get(id_path+name);
-  return response.data["data"][0]["id"];
-}
+  String name_path =
+      "https://api.cricapi.com/v1/players_info?apikey=a7233f4d-1f47-461a-92d5-899b8409091d&id=";
+  getId(String name) async {
+    final response = await dio.get(id_path + name);
+    return getDetails(response.data["data"][0]["id"]);
+  }
+
+  getDetails(id) async {
+    final response = await dio.get(name_path + id);
+    return response.data;
+  }
 }
